b51f436555a7573e1ba6d9050347e99dfd1ebd0b
unknown <vlfmap@gmail.com>
2023-06-07 16:21:51 +0200

    STARTED WRITING OUR OWN ENGINE

---
+++ src/engine/main.js
@@ -0,0 +1,9 @@
+const {VECTOR2D} = require('./math/math');
+
+const ParticleEngine = {
+    math: {
+        vector2d: VECTOR2D
+    }
+};
+
+module.exports = {ParticleEngine};
---
+++ src/engine/math/math.js
@@ -0,0 +1,24 @@
+/**
+ * VECTOR2D | TYPE
+ * x: integer || float
+ * y: integer || float
+ * force:(optional) float: only use as a multiplier.
+ * the size of the vector should determine the force
+ */
+
+class VECTOR2D {
+    x
+    y
+    force
+    constructor(x, y, force=null){
+        this.x = x; this.y = y; force ? this.force = force : null;
+    }
+    get x(){return this.x}; get y(){return this.y};
+    get size(){return Math.sqrt(((this.x*this.x)+(this.y*this.y)))}
+    set x(value){this.x = value};
+    set y(value){this.y = value};
+}
+
+module.exports = {
+    VECTOR2D
+}
